a1362ae340df8d6a947eb136899a2ac0f93af5a3
Kobi Cohen <kobic8@gmail.com>
2022-05-01 22:31:03 +0300

    personal measures part is done, init daily log

--- Nutrition/main_nutrition_log.py
+++ Nutrition/main_nutrition_log.py
@@ -1,16 +1,56 @@
-def init(date):
-    from datetime import datetime
-    import pandas as pd
-    df_date = pd.DataFrame({'last updated:': datetime.now().strftime("%B %d %Y %H:%M")})
-    df_cal_summary = pd.DataFrame({'caloric balance': '0', 'total calories consumed': '0', 'total calories burned:': '0'})
-    # ASSUME: I don't need to build the header lines, and will put them as I build the log
-    dfs = [df_date, df_cal_summary]
-    startrow = 0
-    with pd.ExcelWriter('try_'+filename) as writer:
-        for df in dfs:
-            # FIXME does this updates the sheet? or overrides it?
-            df.to_excel(writer, sheet_name=date, engine="xlsxwriter", startrow=startrow)
-            startrow += (df.shape[0] + 2)
+def init():
+    """
+    This procedure creates an empty Excel file as nutrition log, with an EMPTY sheet for the personal measures.
+
+    """
+    import xlsxwriter as xls
+    workbook = xls.Workbook(filename)
+    sheet = workbook.add_worksheet('measures')
+    headers = ['Last updated', 'Weight', 'Height', 'BMR']
+    for row in range(len(headers)):
+        sheet.write(row, 0, headers[row])
+    workbook.close()
+
+
+def get_measures_from_user():
+    """
+    This procedure gets from the user its personal measures and returns it as a dictionary
+
+    Returns
+    -------
+    measures [dict]: with key-values of: current weight, height, BMR
+    """
+    measures = dict()
+    measures['weight'] = input("How much you weigh now? [kg]\n")
+    measures['height'] = input("What is your height? [cm]\n")
+    measures['BMR'] = input("What is your BMR? [kcal]\n")
+    return measures
+
+
+def update_worksheet_measures(current_measures, date):
+    """
+
+    Parameters
+    ----------
+    current_measures [dict]: user input with key-values of: current weight, height, BMR
+    date [str]: The date and time to be documented as last-update status
+
+    Returns
+    -------
+    This procedure updates the sheet 'measures' with updated measures from the user
+    """
+    import openpyxl as xls
+    workbook = xls.load_workbook(filename)
+    # sheet = workbook.get_sheet_by_name('measures')
+    sheet = workbook['measures']
+    values_to_update = [date] + list(current_measures.values())
+    row = 1
+    col = 'B'
+    for value in values_to_update:
+        sheet[col+str(row)] = value
+        row += 1
+    workbook.save(filename)
+    workbook.close()
 
 
 def read_food_dict():
@@ -28,6 +68,34 @@ def read_food_dict():
     food_names = [value['Food'] for value in food_list]
     food_dict = {row.pop('Food'): row for row in food_list}
     return food_names, food_dict
+
+
+def init_daily_log(date):
+    import openpyxl as xls
+    workbook = xls.load_workbook(filename)
+    sheet = workbook.create_sheet(date)
+    headers = ['Last Updated', '', 'Balance', 'Total Consumed', 'Total Burned', '']
+    for header in headers:
+        sheet.append([header])
+    headers = ['Food', 'Amount', 'Unit', 'Calories', 'Protein', 'Carbs', 'Fats']
+    sheet.append(headers)
+    workbook.save(filename)
+    workbook.close()
+
+
+def init_draft():
+    from datetime import datetime
+    import pandas as pd
+    df_date = pd.DataFrame({'last updated:': datetime.now().strftime("%B %d %Y %H:%M")})
+    df_cal_summary = pd.DataFrame({'caloric balance': '0', 'total calories consumed': '0', 'total calories burned:': '0'})
+    # ASSUME: I don't need to build the header lines, and will put them as I build the log
+    dfs = [df_date, df_cal_summary]
+    startrow = 0
+    with pd.ExcelWriter('try_'+filename) as writer:
+        for df in dfs:
+            # FIXME does this updates the sheet? or overrides it?
+            df.to_excel(writer, sheet_name=date, engine="xlsxwriter", startrow=startrow)
+            startrow += (df.shape[0] + 2)
 
 
 def get_personal_measures():
@@ -94,6 +162,17 @@ def update_personal_measures():
 
 
 if __name__ == '__main__':
+    from os import path
+    from datetime import datetime
     filename = 'nutrition_log.xlsx'
+    if not path.isfile(filename):
+        init()
+        personal_measures = get_measures_from_user()
+        update_date = datetime.now().strftime("%B %d %Y %H:%M")
+        update_worksheet_measures(personal_measures, update_date)
+
     food_dict, food_name_list = read_food_dict()
-    personal_measures = get_personal_measures()
+    update_date = datetime.now().strftime("%B %d %Y %H:%M")
+    date = update_date.split(' 2022')[0]
+    init_daily_log(date)
+
