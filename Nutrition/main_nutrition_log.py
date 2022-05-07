8ea8af268b3e92efb1cad08520d356b9bf6c1206
Kobi Cohen <kobic8@gmail.com>
2022-05-07 22:24:24 +0300

    (1) Convert FoodDictionary.xlsx to Heb (2) Beta version for streamlit (3) TODO: input from user and last update

--- Nutrition/main_nutrition_log.py
+++ Nutrition/main_nutrition_log.py
@@ -1,4 +1,4 @@
-def init():
+def init(filename):
     """
     This procedure creates an empty Excel file as nutrition log, with an EMPTY sheet for the personal measures.
 
@@ -10,6 +10,7 @@ def init():
     for row in range(len(headers)):
         sheet.write(row, 0, headers[row])
     workbook.close()
+    return True
 
 
 def get_measures_from_user():
@@ -24,10 +25,11 @@ def get_measures_from_user():
     measures['weight'] = input("How much you weigh now? [kg]\n")
     measures['height'] = input("What is your height? [cm]\n")
     measures['BMR'] = input("What is your BMR? [kcal]\n")
+    # TODO: consider maybe running a weight-log or even circumference log, after mastering pandas
     return measures
 
 
-def update_worksheet_measures(current_measures, date):
+def update_worksheet_measures(filename, current_measures, date=None):
     """
 
     Parameters
@@ -40,9 +42,12 @@ def update_worksheet_measures(current_measures, date):
     This procedure updates the sheet 'measures' with updated measures from the user
     """
     import openpyxl as xls
+    from datetime import datetime
     workbook = xls.load_workbook(filename)
     # sheet = workbook.get_sheet_by_name('measures')
     sheet = workbook['measures']
+    if date is None:
+        date = datetime.now().strftime("%B %d %Y %H:%M")
     values_to_update = [date] + list(current_measures.values())
     row = 1
     col = 'B'
@@ -51,6 +56,7 @@ def update_worksheet_measures(current_measures, date):
         row += 1
     workbook.save(filename)
     workbook.close()
+    return True
 
 
 def read_food_dict():
@@ -70,7 +76,7 @@ def read_food_dict():
     return food_names, food_dict
 
 
-def init_daily_log(date):
+def init_daily_log(filename, date):
     """
         This procedure creates an empty worksheet related to a specific date, with its related headers.
 
@@ -81,14 +87,16 @@ def init_daily_log(date):
     headers = ['Last Updated', '', 'Balance', 'Total Consumed', 'Total Burned', '']
     for header in headers:
         sheet.append([header])
-    sheet['B3'] = '=B4-B5'  # formula for caloric balance
+    sheet['B3'] = 0  # '=B4-B5' # formula for caloric balance DOES NOT WORK TO READ
+    sheet['B4'] = 0
+    sheet['B5'] = 0
     headers = ['Food', 'Amount', 'Unit', 'Calories', 'Protein', 'Carbs', 'Fats']
     sheet.append(headers)
     workbook.save(filename)
     workbook.close()
 
 
-def read_daily_log(date):
+def read_daily_log(filename, date):
     """
     The procedure checks if there is logged data for a given date. In case not, it creates an empty sheet for that day.
     Returns
@@ -99,9 +107,9 @@ def read_daily_log(date):
     import openpyxl as xls
     from tabulate import tabulate
     workbook = xls.load_workbook(filename)
-    # ASSUME: the food log exists, what if not? try-except?
+    # ASSUME: the file itself exists, what if not? try-except?
     if date not in workbook.get_sheet_names():
-        init_daily_log(date)
+        init_daily_log(filename, date)
         return None  # or 0
     else:
         workbook.close()
@@ -110,7 +118,7 @@ def read_daily_log(date):
     return food_reads
 
 
-def create_rwo_to_add(added_food, amount):
+def create_rwo_to_add(food_dict, added_food, amount):
     food_data = food_dict[added_food]
     row_to_add = list(food_data.values())
     factor = amount / (food_data['Amount'])
@@ -118,20 +126,24 @@ def create_rwo_to_add(added_food, amount):
     return row_to_add
 
 
-def update_food_log(date, row_to_add, startrow=8):
+def update_food_log(filename, date, row_to_add, startrow=8):
     from openpyxl import load_workbook
+    from datetime import datetime
     import pandas as pd
     workbook = load_workbook(filename)
     sheet = workbook[date]
     cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
     for col, value in zip(cols, row_to_add):
         sheet[col+str(startrow)] = value
+    sheet['B1'] = datetime.now().strftime("%B %d %Y %H:%M")
     workbook.save(filename)
     workbook.close()
+    return True
 
 
-def sum_daily_log(date):
-    food_reads = read_daily_log(date)
+def sum_daily_log(filename, date, food_reads=None):
+    if food_reads is None:
+        food_reads = read_daily_log(filename, date)
     headers = ['Protein', 'Carbs', 'Fats']
     total_df = food_reads.sum(axis=0)
     total_values = dict()
@@ -141,18 +153,40 @@ def sum_daily_log(date):
     return total_consumed, total_values
 
 
-def update_total_calories(date, consumed, burned=0):
+def update_total_calories(filename, date, consumed, burned):
     from openpyxl import load_workbook
     from datetime import datetime
     workbook = load_workbook(filename)
     sheet = workbook[date]
     sheet['B1'] = datetime.now().strftime("%B %d %Y %H:%M")
-    if consumed:
-        sheet['B4'] = consumed
-    if burned:
-        sheet['B5'] = burned
+    sheet['B4'] = consumed
+    sheet['B5'] = burned
+    sheet['B3'] = sheet['B4'].value - sheet['B5'].value
     workbook.save(filename)
     workbook.close()
+    return True
+
+
+def get_total_calories(filename, date):
+    from openpyxl import load_workbook
+    workbook = load_workbook(filename)  # data_only=True)
+    sheet = workbook[date]
+    total_cal = dict()
+    keys = ['balance', 'consumed', 'burned']
+    for index, key in enumerate(keys):
+        total_cal[key] = sheet['B'+str(3+index)].value
+    updated = sheet['B1'].value
+    workbook.close()
+    return total_cal, updated
+
+
+def get_last_update(filename, sheet_name):
+    from openpyxl import load_workbook
+    workbook = load_workbook(filename)  # data_only=True)
+    sheet = workbook[sheet_name]
+    updated = sheet['B1'].value
+    workbook.close()
+    return updated
 
 
 def init_draft():
@@ -205,7 +239,7 @@ def read_personal_measures():
         return None
 
 
-def update_personal_measures():
+def update_personal_measures(filename):
     """
     This procedure gets input from the user personal measures and updates the xls file.
     Returns
@@ -237,24 +271,24 @@ if __name__ == '__main__':
     from os import path
     from datetime import datetime
     filename = 'nutrition_log.xlsx'
-    if not path.isfile(filename):
-        init()
-        personal_measures = get_measures_from_user()
-        update_date = datetime.now().strftime("%B %d %Y %H:%M")
-        update_worksheet_measures(personal_measures, update_date)
-
-    food_dict, food_name_list = read_food_dict()
-    update_date = datetime.now().strftime("%B %d %Y %H:%M")
-    date = update_date.split(' 2022')[0]
-    date = 'May 01'
-    init_daily_log(date)
-    startrow = 8
-
-    added_food = 'Bread'
-    amount = 3
-    food_reads = read_daily_log(date)
-    new_row = create_rwo_to_add(added_food, amount)
-    startrow += len(food_reads)
-
-    consumed = 2200
-    burned = 2900
+    # if not path.isfile(filename):
+    #     init()
+    #     personal_measures = get_measures_from_user()
+    #     update_date = datetime.now().strftime("%B %d %Y %H:%M")
+    #     update_worksheet_measures(personal_measures, update_date)
+    #
+    # food_dict, food_name_list = read_food_dict()
+    # update_date = datetime.now().strftime("%B %d %Y %H:%M")
+    # date = update_date.split(' 2022')[0]
+    # date = 'May 01'
+    # init_daily_log(date)
+    # startrow = 8
+    #
+    # added_food = 'Bread'
+    # amount = 3
+    # food_reads = read_daily_log(date)
+    # new_row = create_rwo_to_add(added_food, amount)
+# startrow += len(food_reads)
+    #
+    # consumed = 2200
+    # burned = 2900
---
+++ Nutrition/nutri_app.py
@@ -0,0 +1,196 @@
+def get_measures_from_user():
+    """
+    This procedure gets from the user its personal measures and returns it as a dictionary
+
+    Returns
+    -------
+    measures [dict]: with key-values of: current weight, height, BMR
+    """
+    measures = dict()
+    with st.form("New measures"):
+        measures['weight'] = st.text_input("How much you weigh now? [kg]\n")
+        measures['height'] = st.text_input("What is your height? [cm]\n")
+        measures['BMR'] = st.text_input("What is your BMR? [kcal]\n")
+        check_save = st.form_submit_button("Data is correct, please update the dataset")
+        if not check_save:
+            st.info("Please enter your measures and click to save")
+    return measures
+
+
+def get_date():
+    update_date = datetime.now().strftime("%B %d")
+    st.text(f"Default date to update is for today: {update_date}")
+    change_date = st.checkbox('Click here to change the date')
+    if change_date:
+        date = st.date_input("Choose a day to view?")
+        update_date = date.strftime("%B %d")
+        return update_date
+
+
+def present_day(filename, date):
+    import matplotlib.pyplot as plt
+    # ASSUME: date exists in xls sheets
+    daily_log_df = nut.read_daily_log(filename, date)
+    total_calories, last_update = nut.get_total_calories(filename, date)
+    st.text(f"last update for {date} was on {last_update}")
+    st.text(total_calories)
+    # TODO: Think about converting this to a pd dataframe and present it to table?
+    total_consumed, total_values = nut.sum_daily_log(filename, date, daily_log_df)
+    values = [value/total_consumed*100 for value in total_values.values()]
+    fig, ax = plt.subplots()
+    headers = [header for header in total_values.keys()]
+    # st.text(headers)
+    ax.pie(values, labels=headers, autopct='%1.1f%%')
+    ax.axis('equal')
+    ax.set_title(f" Total of {total_consumed} calories consumed for: {date} ")
+    st.pyplot(fig)
+
+    # values = [value for value in total_calories.values()]
+    # headers = [header for header in total_calories.keys()]
+    # st.bar_chart(values, headers)
+    chart = st.bar_chart(pd.DataFrame(total_calories, index=['Total']))
+    # st.altair_chart(chart)
+
+
+if __name__ == '__main__':
+    import streamlit as st
+    import main_nutrition_log as nut
+    from os import path
+    from datetime import datetime
+    import pandas as pd
+    from matplotlib import pyplot as plt
+
+    # main page
+    st.title("Kobic's Nutrition App")
+    filename = 'nutrition_log.xlsx'
+    initialized = False
+    updated_measures = False
+    if not path.isfile(filename):
+        measures_input = None
+        initialized = nut.init(filename)
+        if initialized:
+            st.success("nutrition_log.xlsx is created")
+            measures_input = get_measures_from_user()
+            if measures_input is not None:
+                updated_measures = nut.update_worksheet_measures(filename, measures_input)
+                if updated_measures:
+                    st.success("Dataset is updated")
+
+    st.header("What would you like to do today?")
+    action = st.radio("Please choose your action", ("View my day", "Update my day", "Update my measures"))
+    if action == "Update my measures":
+        measures_input = None
+        measures_input = get_measures_from_user()
+        if measures_input is not None:
+            nut.update_worksheet_measures(filename, measures_input)
+            st.success("Dataset is updated")
+        st.text(measures_input)
+    if action == "View my day":
+        save_date = False
+        with st.form("Choose date to view"):
+            date = get_date()
+            save_date = st.form_submit_button(f"Save: {date}")
+            # FIXME: what if we choose a date that is not in the xls? we should check and offer
+        if save_date:
+            # st.text(f"Data for {date} was last updated on: {nut.get_last_update(filename, date)}")
+            present_day(filename, date)
+        save_date = False
+
+    if action == "Update my day":
+        # get date to edit
+        with st.form("Choose date to view"):
+            date = get_date()
+            save_date = st.form_submit_button(f"Save: {date}")
+
+        food_names, food_dict = nut.read_food_dict()
+        count = 0
+        startrow = 8
+        daily_log_df = nut.read_daily_log(filename, date)
+        if daily_log_df is not None:
+            count = len(daily_log_df) - startrow
+            count = 0
+            startrow += len(daily_log_df)
+
+        done_button = False
+        # while done_button is False:
+        with st.form("Add Food"):
+            food = []
+            amount = []
+            food = st.selectbox("What did you eat", food_names)
+            if food:
+                units = food_dict[food]['Unit']
+                amount = st.text_input(f"How much [{units}] did you eat?")
+            if amount:
+                amount = float(amount)
+            st.text(f" the amount is {amount} and type: {type(amount)}")
+            save = None
+            save = st.form_submit_button("Save")
+            if save:
+                row_to_add = nut.create_rwo_to_add(food_dict, food, amount)
+                update = nut.update_food_log(filename, date, row_to_add, startrow)
+                daily_log_df = nut.read_daily_log(filename, date)
+                startrow += 1
+                st.text(f"number of items before: {count}")
+                count += 1
+                st.text(f"number of items: {count}")
+                st.text(f"row is: {startrow}")
+                st.dataframe(daily_log_df.iloc[-count:])
+                if update:
+                    st.success("Dataset is updated")
+            next_button = st.form_submit_button("Next")
+            if next_button:
+                food = []
+                amount = []
+                units = []
+                save = []
+                # count += 1
+
+        done_button = st.checkbox("Done")
+        # burned = 0
+        if done_button:
+            refresh = False
+            done_burned_button = False
+            update_cal = False
+            cal_burned = None
+            get_burned = False
+            burned = False
+            with st.form("Calories burned"):
+                burned = st.text_input(f"Update total calories burned on {date}")
+                if burned:
+                    burned = float(burned)
+                    st.text(f"get burned is: {burned}")
+                refresh = st.form_submit_button('Refresh')
+                if refresh:
+                    st.text(f"Burned: {burned}")
+                    total_consumed, total_values = nut.sum_daily_log(filename, date, daily_log_df)
+                    update_cal = nut.update_total_calories(filename, date, total_consumed, burned)
+                    if update_cal:
+                        st.success("Dataset is updated")
+
+            done_burned_button = st.checkbox("Done updating")
+            if done_burned_button:
+                present_day(filename, date)
+            # st.info("Now the total calories consumed will be summed up and presented")
+            # get_burned = st.button(f"Update total calories burned on {date}")
+            # if get_burned:
+            #     st.text(f"get burned is: {get_burned}")
+            #     # st.text(f"Burned: {burned}")
+            #     # burned = st.number_input("Total calories burned")
+            #     burned = False
+            #     burned = st.text_input("Total calories burned")
+            #     if burned:
+            #         st.text(f" burned is: {burned}")
+            #         cal_burned = float(burned)
+            #         st.text(f"Burned: {cal_burned}")
+            # st.text(f"Burned: {cal_burned}")
+            # refresh = st.button('Refresh')
+            # if refresh:
+            #     st.text(f"Burned: {cal_burned}")
+            #     total_consumed, total_values = nut.sum_daily_log(date, daily_log_df)
+            #     update_cal = nut.update_total_calories(filename, date, total_consumed, cal_burned)
+            #     if update_cal:
+            #         present_day(filename, date)
+            # refresh = False
+            # update_cal = False
+
+
